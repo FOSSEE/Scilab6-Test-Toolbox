5f2bf8a9e8c5cd3d9dd4da2635256ef428b89adc
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-02-01 15:02:40 +0530

    Add files via upload

---
+++ test/script.sh
@@ -0,0 +1,4 @@
+gcc main.c
+./a.out
+
+//Shell script to execute the main.c 
---
+++ test/trans.h
@@ -0,0 +1,6 @@
+#ifndef TRANS_DOT_H    
+#define TRANS_DOT_H    
+
+void transpose(int n,int m,int a[][100],int t[][100]);
+
+#endif
