4b90fefde8d10d6de2cc24f6fdaf9629dd38ece9
B C Sagar <62722608+99bcsagar@users.noreply.github.com>
2021-02-15 19:24:55 +0530

    Update trans.c

--- test/trans.c
+++ test/trans.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+#include <stdlib.h>
+#include <string.h>
+int transpose(int i,int j)
+
+    // Finding the transpose of matrix a
+    for (i = 0; i < r; ++i)
+        for (j = 0; j < c; ++j) {
+            transpose[j][i] = a[i][j];
+        }
+
+    // Displaying the transpose of matrix a
+    printf("\nTranspose of the matrix:\n");
+    for (i = 0; i < c; ++i)
+        for (j = 0; j < r; ++j) {
+            printf("%d  ", transpose[i][j]);
+            if (j == r - 1)
+                printf("\n");
+        }
+    return 0;
+}
