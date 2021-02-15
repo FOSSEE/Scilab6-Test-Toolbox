d923fc753e487a3bbbf896e173aff6e72175e86b
B C Sagar <62722608+99bcsagar@users.noreply.github.com>
2021-02-15 19:26:07 +0530

    Update main.c

--- test/main.c
+++ test/main.c
@@ -10,6 +10,7 @@
         for (j = 0; j < c; ++j) {
             printf("Enter element a%d%d: ", i + 1, j + 1);
             scanf("%d", &a[i][j]);
+         int transpose(r,c,a[i][j]);
         }
 
    
